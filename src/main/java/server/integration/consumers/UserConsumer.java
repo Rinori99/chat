159c57e9ba32c09b6369435c5c526e80f9f0929a
Rinori99 <rinori99@live.com>
2020-06-01 00:09:57 +0200

    minor changes

--- src/main/java/server/integration/consumers/UserConsumer.java
+++ src/main/java/server/integration/consumers/UserConsumer.java
@@ -8,15 +8,15 @@ import server.services.UserService;
 @Service
 public class UserConsumer {
 
-//    private UserService userService;
-//
-//    public UserConsumer(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @RabbitListener(queues = {"${queue.user.new.teacher}", "${queue.user.new.parent}"})
-//    public void handleNewUserReception(SerializableUser user) {
-//        userService.saveUser(user.getId(), user.getFirstName(), user.getLastName(), user.getRole());
-//    }
+    private UserService userService;
+
+    public UserConsumer(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RabbitListener(queues = {"${queue.user.new.teacher}", "${queue.user.new.parent}"})
+    public void handleNewUserReception(SerializableUser user) {
+        userService.saveUser(user.getId(), user.getFirstName(), user.getLastName(), user.getRole());
+    }
 
 }
